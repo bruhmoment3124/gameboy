ac937a493b587e0f79d428fd9e235d63272f9904
bruhmoment3124 <73769756+bruhmoment3124@users.noreply.github.com>
2021-06-18 14:39:33 -0400

    2 moving turtles
    2 different turtles are moving

--- mario bros/enemysprites.c
+++ mario bros/enemysprites.c
@@ -8,52 +8,49 @@ struct Enemy
 
 struct Enemy turtle[3]; /* turtle object */
 
-void moveEnemy(UINT8 x, UINT8 y)
-{
-	move_sprite(9, x, y);
-}
-
 void enemySetup()
 {
 	turtle[1].x = 0, turtle[1].y = 24;
 	turtle[2].x = 100, turtle[2].y = 100;
-	turtle[3].x = 20, turtle[3].y = 24;
 	
 	set_sprite_tile(9, 9);
 	set_sprite_tile(10, 9);
 	set_sprite_tile(11, 9);
 	
-	moveEnemy(turtle[1].x, turtle[1].y);
+	move_sprite(9, turtle[1].x, turtle[1].y);
 	move_sprite(10, turtle[2].x, turtle[2].y);
-	move_sprite(11, turtle[3].x, turtle[3].y);
 }
 
 /* enemy movement pattern */
 void enemyMovement()
 {
-	/* collision */
-	if(canEntityMove(turtle[1].x, turtle[1].y-8, 0x26))
-	{
-		turtle[1].y++; /* gravity */
-		moveEnemy(turtle[1].x, turtle[1].y);
-	}
+	int i;
+	for(i=0; i<3; i++)
+	{	
+		/* collision */
+		if(canEntityMove(turtle[i].x, turtle[i].y-8, 0x26))
+		{
+			turtle[i].y++; /* gravity */
+			move_sprite(9+i, turtle[i].x, turtle[i].y);
+		}
 	
-	/* screen wrap */
-	if(turtle[1].x <= 0)
-	{
-		turtle[1].x = 160;
-	} else if(turtle[1].x >= 160)
-	{
-		turtle[1].x = 0;
-	}
+		/* screen wrap */
+		if(turtle[i].x <= 0)
+		{
+			turtle[i].x = 160;
+		} else if(turtle[i].x >= 160)
+		{
+			turtle[i].x = 0;
+		}
 	
-	/* pipes */
-	if(canEntityMove(turtle[1].x, turtle[1].y-16, 0x29))
-	{
-		turtle[1].x = 0;
-		turtle[1].y = 24;
-	}
+		/* pipes */
+		if(canEntityMove(turtle[i].x, turtle[i].y-16, 0x29))
+		{
+			turtle[i].x = 0;
+			turtle[i].y = 24;
+		}
 	
-	turtle[1].x++;
-	moveEnemy(turtle[1].x, turtle[1].y);
+		turtle[i].x++;
+		move_sprite(9+i, turtle[i].x, turtle[i].y);
+	}
 }
